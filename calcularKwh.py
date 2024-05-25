6b7e6c23901b9e78c4ee3b5c7f5483ebad160234
Igor Harder <chulea123@gmail.com>
2024-05-25 19:21:27 -0300

    Update calcularKwh.py

--- calcularKwh.py
+++ calcularKwh.py
@@ -1,4 +1,5 @@
+eletro = str(input('Informe o nome do eletrônico eletrônico:'))
 tempo = float(input('Tempo de uso: '))
-kh_w = 0.90
-gasto = tempo * kh_w
-print(f'O gasto foi de R$ {gasto :.2f}')
+kh_w = 0.90 #Preço do Kw/H
+gasto = tempo * kh_w #calcula o gasto
+print(f'Seu(a) {eletro} teve um gasto de R$ {gasto :.2f} com {tempo} Horas de uso')
