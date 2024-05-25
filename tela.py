f32419bca36682949b407dc56a1b95460e181078
Igor Harder <chulea123@gmail.com>
2024-05-25 19:51:20 -0300

    Tela em python
    Estou tentando personaliza-la

---
+++ tela.py
@@ -0,0 +1,14 @@
+from customtkinter import *
+
+tela = CTk()
+
+
+tela.geometry('800x600') #Tamanho da janela
+tela.title('IgoIgo') #Titulo da pagina
+
+
+set_appearance_mode('dark') #Modo escuro 
+
+
+
+tela.mainloop()
